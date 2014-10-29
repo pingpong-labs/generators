20e829fe3953aff6cc8cdff0721a5e5671fcb372
gravitano <gue.warsono@gmail.com>
2014-10-29 11:19:47 +0700

    Added SeedGenerator

---
+++ src/Pingpong/Generators/SeedGenerator.php
@@ -0,0 +1,15 @@
+<?php namespace Pingpong\Generators;
+
+class SeedGenerator extends FormRequestGenerator {
+
+    /**
+     * @var string
+     */
+    protected $type = 'seed';
+
+    /**
+     * @var string
+     */
+    protected $stub = 'seed';
+
+}
---
+++ tests/SeedGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Pingpong\Generators\SeedGenerator;
+
+class SeedGeneratorTest extends TestCase {
+
+	public function testGenerateSeed()
+	{
+		$generator = new SeedGenerator($this->path, 'UsersTableSeeder');
+
+		$generator->generate();
+
+		$this->assertGenerated($generator);
+	}
+
+	public function testGenerateWithSpecifiedOptions()
+	{
+		$options = ['namespace' => 'Database\\Seeds'];
+
+		$generator = new SeedGenerator($this->path, 'PostsTableSeeders', $options);
+
+		$generator->generate();
+
+		$this->assertGenerated($generator);
+	}
+
+}
