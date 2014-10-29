b9b2d68a49e3f0e58770836f082ec95893801c85
gravitano <gue.warsono@gmail.com>
2014-10-29 11:15:47 +0700

    Added FormRequestGenerator

---
+++ tests/FormRequestGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Pingpong\Generators\FormRequestGenerator;
+
+class FormRequestGeneratorTest extends TestCase {
+
+	public function testGenerateCommand()
+	{
+		$generator = new FormRequestGenerator($this->path, 'LoginRequest');
+
+		$generator->generate();
+
+		$this->assertGenerated($generator);
+	}
+
+	public function testGenerateWithSpecifiedOptions()
+	{
+		$options = ['namespace' => 'Http\\Request\\Auth'];
+
+		$generator = new FormRequestGenerator($this->path, 'RegisterRequest', $options);
+
+		$generator->generate();
+
+		$this->assertGenerated($generator);
+	}
+
+}
