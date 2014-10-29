cd72d30d73e8f0c427b3bee7d86d5562fd49b349
gravitano <gue.warsono@gmail.com>
2014-10-29 11:25:12 +0700

    Added ProviderGenerator

---
+++ src/Pingpong/Generators/ProviderGenerator.php
@@ -0,0 +1,15 @@
+<?php namespace Pingpong\Generators;
+
+class ProviderGenerator extends FormRequestGenerator {
+
+    /**
+     * @var string
+     */
+    protected $type = 'provider';
+
+    /**
+     * @var string
+     */
+    protected $stub = 'provider';
+
+}
