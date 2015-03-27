cda0a9cd1e64321d0da3f315c79fdda4c7bc8864
Gravitano <gravitano16@gmail.com>
2015-03-27 09:52:10 +0700

    add support for converting string of migration to a stub script

--- Parsers/MigrationParser.php
+++ Parsers/MigrationParser.php
@@ -2,7 +2,9 @@
 
 namespace Pingpong\Generators\Parsers;
 
-class MigrationParser {
+use Illuminate\Contracts\Support\Arrayable;
+
+class MigrationParser implements Arrayable {
 
 	/**
 	 * The array of custom migrtions.
@@ -15,6 +17,23 @@ class MigrationParser {
 	];
 
 	/**
+	 * The migration schema.
+	 *
+	 * @var string
+	 */
+	protected $migration;
+
+	/**
+	 * Create new instance.
+	 * 
+	 * @param string|null $migration
+	 */
+	public function __construct($migration = null)
+	{
+		$this->migration = $migration;
+	}
+
+	/**
 	 * Parse a string to array of formatted schema.
 	 * 
 	 * @param  string $migration]
@@ -22,11 +41,11 @@ class MigrationParser {
 	 */
 	public function parse($migration)
 	{
-		$schemas = explode(',', str_replace(' ', '', $migration));
+		$this->migration = $migration;
 
 		$parsed = [];
 
-		foreach ($schemas as $schema)
+		foreach ($this->getSchemas() as $schema)
 		{
 			$column = $this->getColumn($schema);
 			
@@ -36,6 +55,75 @@ class MigrationParser {
 		}
 
 		return $parsed;
+	}
+
+	public function getSchemas()
+	{
+		return explode(',', str_replace(' ', '', $this->migration));
+	}
+
+	/**
+	 * Convert string migration to array.
+	 * 
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return $this->parse($this->migration);
+	}
+
+	/**
+	 * Render the migration to formatted script.
+	 * 
+	 * @return string
+	 */
+	public function render()
+	{
+		$results = '';
+
+		foreach ($this->toArray() as $column => $attributes)
+		{
+			$results .= $this->createField($column, $attributes); 
+		}
+
+		return $results;
+	}
+
+	/**
+	 * Create field.
+	 * 
+	 * @param  string $column
+	 * @param  array $attributes
+	 * @return string
+	 */
+	public function createField($column, $attributes)
+	{
+		$results = '$table';
+
+		foreach ($attributes as $key => $field)
+		{
+			$results .= $this->formatField($key, $field, $column);
+		}
+
+		return $results .= ';'.PHP_EOL;
+	}
+
+	/**
+	 * Format field to script.
+	 * 
+	 * @param  int $key
+	 * @param  string $field
+	 * @param  string $column
+	 * @return string
+	 */
+	protected function formatField($key, $field, $column)
+	{
+		if($key == 0)
+		{
+			return '->' . $field . "('". $column."')";	
+		}
+
+		return '->' . $field . '()';
 	}
 
 	/**
