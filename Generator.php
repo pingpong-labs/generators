9656f0d87d6a7b6a91c276cd3f77d00b3ba1b5ef
Gravitano <gravitano16@gmail.com>
2016-01-05 17:17:42 +0700

    remove setStubBasePath method

--- Generator.php
+++ Generator.php
@@ -78,24 +78,12 @@ abstract class Generator
     }
 
     /**
-     * Set stub base path.
-     *
-     * @return void 
-     */
-    protected function setStubBasePath()
-    {
-        Stub::setBasePath(config('generators.template_path', __DIR__.'/Stubs').'/');
-    }
-
-    /**
      * Get stub template for generated file.
      *
      * @return string
      */
     public function getStub()
     {
-        $this->setStubBasePath();
-
         $stub = new Stub($this->stub.'.stub', $this->getReplacements());
 
         return $stub->render();
