dd573d49e3997067ac0cc802adf045926ba26c19
Gravitano <gravitano16@gmail.com>
2016-01-05 10:48:28 +0700

    setup path to exception class

--- FileAlreadyExistsException.php
+++ FileAlreadyExistsException.php
@@ -6,4 +6,31 @@ use Exception;
 
 class FileAlreadyExistsException extends Exception
 {
+    /**
+     * The file path.
+     * 
+     * @var string
+     */
+    private $path;
+
+    /**
+     * Create a new instance of this class.
+     * 
+     * @param string $path
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+        parent::__construct('File already exist at path: '.$path);
+    }
+
+    /**
+     * Gets the value of path.
+     *
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
 }
